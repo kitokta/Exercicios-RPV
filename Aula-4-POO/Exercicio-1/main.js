33769f20a26678adc03dc17e98ede8b0ce7a2cc6
Gabi Zanini <psicologia.plus@gmail.com>
2022-10-13 20:03:16 -0300

    Ex1/aula4 conta do banco

--- Aula-4-POO/Exercicio-1/main.js
+++ Aula-4-POO/Exercicio-1/main.js
@@ -1,11 +1,24 @@
-//Exercicio 1
-const num1 = parseFloat(prompt("Digite o primeiro número:"));
-const num2 = parseFloat(prompt("Digite o o segundo número:"));
+class ContaBanco {
+    constructor(nome, saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
 
-if (num1>num2) {
-    alert(`O maior número é: ${num1}`)
-} else if (num2>num1) {
-    alert(`O maior número é: ${num2}`)
-} else {
-    alert("Os números são iguais");
+    deposito(quantidade) {
+        this.saldo += quantidade;
+    }
+
+    saque(quantidade) {
+        this.saldo = this.saldo - quantidade;
+    }
 }
+
+let conta = new ContaBanco('Gab', 10000);
+
+console.log(conta);
+
+conta.deposito(2000);
+console.log(conta);
+
+conta.saque(10000);
+console.log(conta)
