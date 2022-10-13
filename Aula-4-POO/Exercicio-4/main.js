f8854baa2f3a842531cf1599a1a89cda7ab304f8
Gabi Zanini <psicologia.plus@gmail.com>
2022-10-13 20:43:15 -0300

    Ex4/aula4 carro e gasolina

--- Aula-4-POO/Exercicio-4/main.js
+++ Aula-4-POO/Exercicio-4/main.js
@@ -1,24 +1,54 @@
-//Exercicio 4
- const arrayNum = []
+class Carro {
+  constructor(marca, cor, gasolina) {
+    this.marca = marca;
+    this.cor = cor;
+    this.gasolina = gasolina;
+  }
 
- arrayNum.push(parseFloat(prompt("Digite o primeiro numero")));
- arrayNum.push(parseFloat(prompt("Digite o segundo numero")));
- arrayNum.push(parseFloat(prompt("Digite o terceiro numero")));
+  static sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
 
- const maiorNum = arrayNum.reduce((maiorNum, numAtual) => {
-    if (numAtual >= maiorNum) {
-        return numAtual;
-    } else {
-        return maiorNum;
+  //dirigie e consome 1L de gasolina por segundo, mostrando no console
+  async dirigir() {
+    while (this.gasolina > 0) {
+      console.log(this.gasolina);
+      this.gasolina--;
+      await Carro.sleep(1000);
+      //se gasolina restando for <= 10L pergunta se quer abastecer
+      if (this.gasolina === 10) {
+        let abastecer = prompt(`Gasolina atual: ${this.gasolina}L\nVocê gostaria de abastecer? (S) ou (N)`);
+        if (abastecer != "n" && abastecer != "N") {
+          this.abastecer();
+          break;
+        }
+      } else if (this.gasolina <= 5) {
+        //avisa que está na reserva se <= 5L
+        alert("SEU CARRO ESTÁ NA RESERVA");
+        let abastecer = prompt("Você gostaria de abastecer? (S) ou (N)");
+        if (abastecer != "n" && abastecer != "N") {
+          this.abastecer();
+          break;
+        }
+      }
     }
- }, arrayNum[0])
+  }
 
- const menorNum = arrayNum.reduce((menorNum, numAtual) => {
-    if (numAtual <= menorNum) {
-        return numAtual;
-    } else {
-        return menorNum;
+  abastecer() {
+    const quantidade = Number(prompt("Quanto você quer abastecer? (L)"));
+    this.gasolina += quantidade;
+
+    const dirigir = prompt("Você gostaria de continuar dirigindo? (S) ou (N)");
+    if (dirigir != "n" && dirigir != "N") {
+      this.dirigir();
     }
- }, arrayNum[0])
+  }
+}
 
- alert(`O maior número é: ${maiorNum}\n O menor número é: ${menorNum}`);
+const gasolinaInicial = Number(
+  prompt("Quantos litros de gasolina o carro possui?")
+);
+
+const carro = new Carro("Honda", "Preto", gasolinaInicial);
+
+carro.dirigir();
