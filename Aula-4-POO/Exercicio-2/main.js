223157eaf774407c8ea1dc6b760c823fa5724717
Gabi Zanini <psicologia.plus@gmail.com>
2022-10-15 16:00:38 -0300

    Adicionado verificacao ao adicionar um item, se ele ja existe adiciona quantidade e atualiza valor do objeto, se nao cria um novo objeto a lista de itens

--- Aula-4-POO/Exercicio-2/main.js
+++ Aula-4-POO/Exercicio-2/main.js
@@ -1,78 +1,94 @@
 class ShoppingCart {
-    constructor() {
-        this.itens = [];
-        this.quantidadeTotal = 0;
-        this.valorTotal = 0;
+  constructor() {
+    this.itens = [];
+    this.quantidadeTotal = 0;
+    this.valorTotal = 0;
+  }
+
+  adicionar(item, quantidade, valor) {
+    let { itens } = this;
+
+    //verificar se item já existe no carrinho
+    let existeItem = false;
+    let index;
+    for (let i = 0; i < itens.length; i++) {
+      if (itens[i].nome.includes(item)) {
+        existeItem = true;
+        //index de onde está o item.
+        index = i;
+        break;
+      }
     }
-    
 
-    adicionar(item, quantidade, valor) {
-        let { itens } = this;
-        
-        itens.push({
-            nome: item,
-            quantidade: quantidade,
-            valorUnitario: valor,
-            valor: valor * quantidade
-        })
-
-        this.quantidadeTotal = ShoppingCart.calcQuantidade(itens);
-            
-        this.valorTotal = ShoppingCart.calcValor(itens)
+    //se nao existir o item cria um novo objeto, se nao adiciona ao existente:
+    if (!existeItem) {
+      itens.push({
+        nome: item,
+        quantidade: quantidade,
+        valorUnitario: valor,
+        valor: valor * quantidade,
+      });
+    } else {
+        itens[index].quantidade += quantidade;
+        itens[index].valor = valor * itens[index].quantidade;
     }
 
-    remover(item, quantidadeDeletar) {
-        let { itens } = this;
+    this.quantidadeTotal = ShoppingCart.calcQuantidade(itens);
 
-        for (let index in itens) {
-            if (itens[index].nome === item) {
-                itens[index].valor = itens[index].valor - (itens[index].valorUnitario * quantidadeDeletar)
-                itens[index].quantidade = itens[index].quantidade - quantidadeDeletar;
-                if(itens[index].quantidade <= 0) {
-                    itens.splice(index, 1);
-                }
-            }
+    this.valorTotal = ShoppingCart.calcValor(itens);
+  }
+
+  remover(item, quantidadeDeletar) {
+    let { itens } = this;
+
+    for (let index in itens) {
+      if (itens[index].nome === item) {
+        itens[index].valor =
+          itens[index].valor - itens[index].valorUnitario * quantidadeDeletar;
+        itens[index].quantidade = itens[index].quantidade - quantidadeDeletar;
+        if (itens[index].quantidade <= 0) {
+          itens.splice(index, 1);
         }
-
-        this.quantidadeTotal = ShoppingCart.calcQuantidade(itens);
-            
-        this.valorTotal = ShoppingCart.calcValor(itens)
+      }
     }
 
+    this.quantidadeTotal = ShoppingCart.calcQuantidade(itens);
 
-    static calcValor(itens) {
-            let soma = 0;
-            for(let index in itens) {
-                soma += itens[index].valor
-            }
-            return soma ;
-    }
+    this.valorTotal = ShoppingCart.calcValor(itens);
+  }
 
-    static calcQuantidade(itens) {
-        let soma = 0;
-            for(let index in itens) {
-                soma += itens[index].quantidade;
-        }
-        return soma;
+  static calcValor(itens) {
+    let soma = 0;
+    for (let index in itens) {
+      soma += itens[index].valor;
     }
-    
+    return soma;
+  }
 
-    imprimirCarrinho() {
-        console.log(this)
+  static calcQuantidade(itens) {
+    let soma = 0;
+    for (let index in itens) {
+      soma += itens[index].quantidade;
     }
+    return soma;
+  }
+
+  imprimirCarrinho() {
+    console.log(this);
+  }
 }
 
 let carrinho = new ShoppingCart();
 
-carrinho.adicionar('PS5', 2, 4000);
-carrinho.adicionar('Canetas', 5, 25);
-carrinho.adicionar('Caderno', 12, 42);
-carrinho.adicionar('Celular', 2, 1000);
+carrinho.adicionar("PS5", 2, 4000);
+carrinho.adicionar("PS5", 2, 4000);
+carrinho.adicionar("Canetas", 5, 25);
+carrinho.adicionar("Caderno", 12, 42);
+carrinho.adicionar("Celular", 2, 1000);
 
 carrinho.imprimirCarrinho();
 
-carrinho.remover('PS5', 1)
-carrinho.remover('Caderno', 10)
-
+carrinho.remover("PS5", 1);
+carrinho.remover("Caderno", 10);
 
 carrinho.imprimirCarrinho();
