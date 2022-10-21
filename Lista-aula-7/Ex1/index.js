81784c8f96c0c28e643cd2fd7b8d505aad7c53fe
Gabi Zanini <psicologia.plus@gmail.com>
2022-10-20 23:09:37 -0300

    Ex 1 pronto

---
+++ Lista-aula-7/Ex1/index.js
@@ -0,0 +1,42 @@
+class Empregado {
+  constructor(nome, sobrenome, salarioMensal) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.salarioMensal = salarioMensal;
+  }
+
+  //altera somente um atributo
+  setAtributo(atributo, novoValor) {
+    this[atributo] = novoValor;
+  }
+
+  //busca somente um atributo
+  getAtributo(atributo) {
+    return this[atributo];
+  }
+
+  //metodo estatico da classe pra aumento geral
+  static setAumento(listaEmpregados) {
+    for (let index in listaEmpregados) {
+      listaEmpregados[index].salarioMensal +=
+        listaEmpregados[index].salarioMensal * 0.1;
+    }
+    console.log(listaEmpregados);
+  }
+
+  //metodo para buscar dados de todos empregados
+  static getEmpregados(listaEmpregados) {
+      console.log(listaEmpregados);
+  }
+}
+
+let listaEmpregados = [];
+
+listaEmpregados.push(new Empregado("Gb", "Zanini", 10000));
+listaEmpregados.push(new Empregado("Julia", "Amaral", 15500));
+listaEmpregados[0].setAtributo("nome", "Gab");
+
+Empregado.getEmpregados(listaEmpregados);
+Empregado.setAumento(listaEmpregados);
+Empregado.getEmpregados(listaEmpregados);
+
